5745b2889466e7461f4ff494f811d1a77861ff8a
jennguyen-1 <nguyenjp7@vcu.edu>
2025-11-25 23:50:25 -0500

    update

--- main.js
+++ main.js
@@ -69,7 +69,21 @@ function showSlides(n) {
    $( "#dialog3" ).dialog( "open" );
 });
 
-
+window.addEventListener("load", function() {
+  const form = document.getElementById('my-form');
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const data = new FormData(form);
+    const action = e.target.action;
+    fetch(action, {
+      method: 'POST',
+      body: data,
+    })
+    .then(() => {
+      alert("Success!");
+    })
+  });
+});
 
 
   // typing text main websitte
